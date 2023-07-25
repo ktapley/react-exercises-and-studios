7b0dcf61fecac52cb1fb92641037fa3e4f9158fd
Katy Tapley <katy.tapley@gmail.com>
2023-07-25 15:36:52 -0500

    Completed React Studio Part3

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      {label: "Quick and Easy", value: "Quick and Easy"},
+      {label: "Sides and Dips", value: "Sides and Dips"},
+      {label: "Mexican", value: "Mexican"}
+   ];
+
+   const [boardName, setName] = useState('no boards yet!');
 
    const handleChange = (event) => {
+      setName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => (
+            <option value={board.value}>{board.label}</option>
+         ))}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://downshiftology.com/";
+   let authorPhoto = "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/07/Lisa-Bryan-square-4.jpg";
+   let authorName = "Lisa Bryan";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Author Photo" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ['avocados', 'tomatoes', 'lime', 'onion', 'garlic'];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,10 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Best Ever Guacamole</h1>
+            <p>This is the best guacamole recipe as it’s simple to make and uses fresh, high-quality ingredients. 
+               Authentic guacamole doesn’t contain fillers and unnecessary ingredients. All you need is avocados, onion, 
+               tomatoes, cilantro, jalapeno pepper, lime juice, garlic, and salt. Easy and delicious!</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +49,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/04/Guacamole-2-2.jpg" alt="Recipe Photo" className="imageUpdates"/>
    );
 }
 
